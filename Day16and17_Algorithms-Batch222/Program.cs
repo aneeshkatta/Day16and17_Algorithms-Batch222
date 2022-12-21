177a9745a9710f9a97fb6a554db8752147381744
Aneesh <katta.aneesh597@gmail.com>
2022-12-21 18:55:53 +0530

    added anagram program

---
+++ Day16and17_Algorithms-Batch222/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16and17_Algorithms_Batch222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram.Check_Anagram();
+        }
+    }
+}
